5fbc532a12e2749453b8c579e0210f93736dde56
Luana <lualuna182@gmail.com>
2021-03-08 15:16:31 -0300

    De objeto para Array com Spread

--- map/unique-values-map.js
+++ map/unique-values-map.js
@@ -21,3 +21,17 @@ const categoryNewSet = new Set(menu.map((cat) => {
     return cat.category
 }))
 console.log(categoryNewSet)
+
+//Ele retorna como objeto, basta usar o spread operator para retornar tudo em array.
+
+const categoryNewSet2 = [...new Set(menu.map((cat) => {
+    return cat.category
+}))]
+console.log(categoryNewSet2)
+
+// COLOCANDO NO HTML
+const resultado = document.querySelector(".result")
+resultado.innerHTML = categoryNewSet2.map((category) => {
+    return `<button>${category}</button>`
+}).join('')
+//ele retorna em array, então é preciso usar o join pra tirar as vírgulas no HTML.
